84df4839a4375501810246c0283b29af8724911e
dbhan08 <38201130+dbhan08@users.noreply.github.com>
2019-12-12 20:22:23 -0800

    Add files via upload

---
+++ Student.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include "Student.h"
+#include <cstring>
+
+using namespace std;
+
+Student::Student() {
+
+
+}
+
+Student::~Student() {
+	delete &firstName;
+	delete &lastName;
+
+}
+
+char* Student::getFirst() {
+	return firstName;
+
+}
+
+char* Student::getLast() {
+return lastName;
+
+}
+
+int Student::getId() {
+return id;
+
+}
+
+float Student::getGpa() {
+return gpa;
+
+}
---
+++ Student.h
@@ -0,0 +1,26 @@
+
+#define STUDENT_H
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class Student {
+public:
+Student();
+char* getFirst();
+char* getLast();
+int getId();
+float getGpa();
+~Student();
+private:
+char* firstName;
+char* lastName;
+float gpa;
+int id;
+};
+
+
+
+
